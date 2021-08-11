4def360eabd8c916e4738a32e3d0a5b46f13e539
evencoding <88191233+evencoding@users.noreply.github.com>
2021-08-11 17:53:46 +0900

    # 2021-08-11
    Challenge Day 3

--- note.py
+++ note.py
@@ -1,2 +1,17 @@
-from math import fsum as sexy_sum
+my_english_dict = {}
 
+def add_to_dict(key, item, defi=None):
+    if(type(key) != dict):
+        print("You need to send a dictionary. You sent:", type(key))
+    elif(defi == None):
+        print("You need to send a word and a definition")
+    elif(item in key):
+        print(f"{item} is already on the dictionary. Won't add.")
+    else:
+        my_english_dict[f"{item}"] = defi
+        print(f"{item} has been added.")
+
+
+add_to_dict(my_english_dict, "kimchi", "asd")
+print(my_english_dict)
+print(my_english_dict["kimchi"])
